ce21a7cfa707007a17e1bd9d6659bdf303f7e433
deepadangi01 <dangideepa1999@gmail.com>
2024-08-29 15:24:45 -1200

    solv ques

---
+++ dic.py
@@ -0,0 +1,24 @@
+# dict1={
+#     123:["ajay",7899564231],
+#     456:["vijay",589644444],
+#     789:["sagar",526874364]
+# }
+# dict1[789][1]=9932489545
+# print(dict1)
+# dict1[564]=["deepa",45632158]
+# print(dict1)
+# dict1.update({123:["xyz",20036589]})
+# print(dict1)
+# dict1[456].pop()
+# print(dict1)
+
+r={}
+n=int(input("how many record you want to ??"))
+for i in range(n):
+    addhar=int(input("enter your adhar no "))
+    name=input("enter your name ")
+    mob=input("enter your mobile no ")
+    d={"name":name,"mobile":mob}
+    r[addhar]=d
+print(r)
+
